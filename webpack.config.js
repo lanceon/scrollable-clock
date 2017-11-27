3ca766f5222dc81e812292fca302cf65fd961984
Alexey Kutas <lkatartn@gmail.com>
2017-11-28 00:20:03 +0200

    Added typescript. First .ts file added, everything works without changes in code

--- webpack.config.js
+++ webpack.config.js
@@ -6,37 +6,36 @@ const config = {
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     hot: true,
-    port: 3333
+    port: 3333,
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
-  entry: [
-    'babel-polyfill',
-    'react-hot-loader/patch',
-    './app/initialize.jsx'
-  ],
+  entry: ['babel-polyfill', 'react-hot-loader/patch', './app/initialize.jsx'],
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'app.js'
+    filename: 'app.js',
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
-        use: [
-          'babel-loader',
-        ]
-      }
-    ]
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules)/,
+        use: ['ts-loader'],
+      },
+    ],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: 'app/assets/index.html'
+      template: 'app/assets/index.html',
     }),
-  ]
+  ],
 };
 
 module.exports = config;
